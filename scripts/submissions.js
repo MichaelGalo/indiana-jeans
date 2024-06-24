641564833af57bf710679ceab49ed001f4cd5a95
Michael Galo <michael@michaelthinks.co>
2024-06-24 13:17:35 -0500

    replaced forof loops with .map and .join methods

--- scripts/submissions.js
+++ scripts/submissions.js
@@ -3,16 +3,12 @@ export const SubmissionList = async () => {
   const response = await fetch("http://localhost:8088/submissions");
   const submissions = await response.json();
 
-  let submissionListHTML = "";
-
-  // Iterate the submissions and create some <section> representations
-  for (const submission of submissions) {
-    submissionListHTML += `<section class="submission">
+  return submissions
+    .map((submission) => {
+      return `<section class="submission">
         <div>Owns Jeans? ${submission.ownsBlueJeans}</div>
         <div>Area type foreign key?${submission.socioLocationId}</div>
     </section>`;
-  }
-
-  // Return the HTML string
-  return submissionListHTML;
+    })
+    .join("");
 };
